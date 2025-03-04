927a05db0973964ec6c757444d003106ff26cc86
Phạm Thanh Sơn <phamthanhson@MacBook-Pro-cua-Pham.local>
2025-03-04 09:26:47 +0700

    fix signup, xoa nodemodules

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -15,29 +15,27 @@ const signUp = async (req, res) => {
     const userExist = await User.findOne({ email });
     if (userExist) {
       console.log("User already exists");
-      res.status(400).json({ message: "User already exists" });
+      res.status(403).json({ message: "User already exists" });
       return;
     }
-
+    res.status(200).json({ message: "User created successfully", newUser });
     await newUser.save();
-    res.status(201).json({ message: "User created successfully", newUser });
-    // console.log(res.data.message);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 };
-const signIn = async (req,res) => {
-    const { email, password } = req.body;
-    const userExist = await User.findOne({email});
-    if(!userExist){
-        res.status(400).json({message: "User not found"});
-        return;
-    }
-    if(userExist.password !== password){
-        res.status(400).json({message: "Login failed"});
-        return;
-    }
+const signIn = async (req, res) => {
+  const { email, password } = req.body;
+  const userExist = await User.findOne({ email });
+  if (!userExist) {
+    res.status(400).json({ message: "User not found" });
+    return;
+  }
+  if (userExist.password !== password) {
+    res.status(400).json({ message: "Login failed" });
+    return;
+  }
 };
 
 export { signUp };
